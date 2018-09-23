9b03cc97fa674f4a1bcdde27d824049f58432601
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-23 20:32:24 -0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <img src="/img/logo.png"></img>
+      </header>
+    )
+  }
+}
+
+export default Header;
