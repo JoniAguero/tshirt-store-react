7e7a131b01b4b09f4823bdef7264d282aa99a888
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-23 20:39:32 -0300

    cambios

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 
-export class Header extends Component {
+export default class Header extends Component {
   render() {
     return (
       <header>
-        <img src="/img/logo.png"></img>
+        <img src="./img/logo.png"></img>
       </header>
     )
   }
 }
-
-export default Header;
