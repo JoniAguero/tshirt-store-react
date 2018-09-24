014fa350f059f56ba005756ec68078894ac9e526
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-24 08:45:49 -0300

    Navegacion (router)

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import data from "../data/data.json";
 
 import Header from './Header/Header';
+import Navegacion from './Navegacion/Navegacion';
 import { Nosotros } from './Nosotros/Nosotros';
 import { NotFound } from './404/404';
 import { Productos } from './Productos/Productos';
@@ -28,12 +29,17 @@ export class Router extends Component {
       <BrowserRouter>
         <div className="contenedor">
           <Header />
+          <Navegacion />
           {/* Nos permite crear las rutas */}
           <Switch>
             <Route exact path="/" render={ () => (
               <Productos productos={this.state.productos}/>
             )} />
+            <Route exact path="/productos" render={() => (
+              <Productos productos={this.state.productos} />
+            )} />
             <Route exact path="/nosotros" component={Nosotros} />
+            <Route exact path="/contacto" component={Nosotros} />
             <Route exact path="/producto/:id" render={(props) => {
               let idProducto = props.location.pathname.replace('/producto/', '');
               return(
