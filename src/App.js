e3d6ab5ae1df78ebdb8e51287aa42e6a35cd7c9d
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-23 22:39:09 -0300

    productos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Router } from './components/Router';
 
-import data from "./data/data.json";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router data={data} />
+        <Router />
       </div>
     );
   }
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import data from "../data/data.json";
+
 import { Inicio } from './Inicio/Inicio'
 import { Nosotros } from './Nosotros/Nosotros';
 import { NotFound } from './404/404';
+import { Productos } from './Productos/Productos';
 
 export class Router extends Component {
 
@@ -13,7 +16,7 @@ export class Router extends Component {
 
   componentWillMount() {
     this.setState({
-      productos: this.props.data
+      productos: data
     })
   }
 
@@ -23,7 +26,9 @@ export class Router extends Component {
       <BrowserRouter>
         {/* Nos permite crear las rutas */}
         <Switch>
-          <Route exact path="/" component={Inicio} />
+          <Route exact path="/" render={ () => (
+            <Productos productos={this.state.productos}/>
+          )} />
           <Route exact path="/nosotros" component={Nosotros} />
           <Route component={ NotFound } />
         </Switch>
